9ad32082c00957cdaf1982f4d08fac79045b21fb
Anton Eliseev <anton2008.e@yandex.ru>
2020-10-01 22:22:07 +0400

    change IRepository interface

---
+++ ToDoApp/Model/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoApp.Model.Interfaces
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll(); 
+        
+        void Add(T item);
+        void Update(T item);
+        void Remove(T item);
+    }
+}
