d418fa6b883c7153c378d2819ce9029588eb3473
Anton Eliseev <anton2008.e@yandex.ru>
2020-10-20 14:42:05 +0400

    fix lists` duplication bug

--- ToDoApp/View/AppDictionary.xaml.cs
+++ ToDoApp/View/AppDictionary.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ToDoApp.View
 {
@@ -15,13 +16,13 @@ namespace ToDoApp.View
             InitializeComponent();
         }
 
-        private void ToDoListItemTextBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void ToDoListItemTextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var nameTextBox = sender as TextBox;
-            nameTextBox.IsReadOnly = false;            
+            nameTextBox.IsReadOnly = false;
         }
 
-        private void ToDoListItemTextBox_LostFocus(object sender, RoutedEventArgs e)
+        private void ToDoListItemTextBox_LostKeyboardFocus(object sender, RoutedEventArgs e)
         {
             var nameTextBox = sender as TextBox;
             nameTextBox.IsReadOnly = true;
