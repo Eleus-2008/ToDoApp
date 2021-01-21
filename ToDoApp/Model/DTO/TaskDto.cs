69e76e5c37b854af8b2fae9f1df3438f46c512bc
Anton Eliseev <anton2008.e@yandex.ru>
2021-01-21 16:41:39 +0400

    add DTOs

---
+++ ToDoApp/Model/DTO/TaskDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ToDoApp.Model;
+
+namespace ToDoAppWebService.DTO
+{
+    public class TaskDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime? Date { get; set; }
+        public TimeSpan? TimeOfBeginning { get; set; }
+        public TimeSpan? TimeOfEnd { get; set; }
+        public int Priority { get; set; }
+        public RepeatingConditions RepeatingConditions { get; set; }
+        
+        public Guid ToDoListId { get; set; } 
+    }
+}
---
+++ ToDoApp/Model/DTO/ToDoListDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoAppWebService.DTO;
+
+namespace ToDoApp.Model.DTO
+{
+    public class ToDoListDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<TaskDto> Tasks { get; set; }
+    }
+}
