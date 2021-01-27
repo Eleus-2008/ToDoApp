b989a2d58f6c55c545a1d57fbe7e21b2103d731a
Anton Eliseev <anton2008.e@yandex.ru>
2021-01-27 23:34:52 +0400

    add authentication`s windows

---
+++ ToDoApp/View/LoginWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using ToDoApp.ViewModel;
+
+namespace ToDoApp.View
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow(LoginViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (((LoginViewModel)DataContext).IsSuccess)
+            {
+                Close();
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ ToDoApp/View/RegisterWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using ToDoApp.ViewModel;
+
+namespace ToDoApp.View
+{
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow(RegisterViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (((RegisterViewModel)DataContext).IsSuccess)
+            {
+                Close();
+            }
+        }
+    }
+}
