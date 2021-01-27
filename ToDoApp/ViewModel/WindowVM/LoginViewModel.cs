54ecd667ce54a825b2ed7447d51f1719c627352f
Anton Eliseev <anton2008.e@yandex.ru>
2021-01-27 23:34:24 +0400

    add viewmodels for authentication`s windows

---
+++ ToDoApp/ViewModel/WindowVM/LoginViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using ToDoApp.Annotations;
+using ToDoApp.Model;
+using ToDoApp.Model.Interfaces;
+
+namespace ToDoApp.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private readonly IAuthentication _authentication;
+        
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool IsSuccess { get; private set; }
+
+        public LoginViewModel(IAuthentication authentication)
+        {
+            _authentication = authentication;
+        }
+
+        private AsyncRelayCommand<object> _loginCommand;
+
+        public AsyncRelayCommand<object> LoginCommand
+        {
+            get
+            {
+                return _loginCommand ??
+                       (_loginCommand = new AsyncRelayCommand<object>(async obj =>
+                       {
+                           try
+                           {
+                               var result = await _authentication.Login(Username, Password);
+                               IsSuccess = result.isSuccess;
+                           }
+                           catch
+                           {
+                               IsSuccess = false;
+                           }
+                       }));
+            }
+        }
+        
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ToDoApp/ViewModel/WindowVM/RegisterViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using ToDoApp.Annotations;
+using ToDoApp.Model;
+using ToDoApp.Model.Interfaces;
+
+namespace ToDoApp.ViewModel
+{
+    public class RegisterViewModel : INotifyPropertyChanged
+    {
+        private readonly IAuthentication _authentication;
+        
+        private string _username;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool IsSuccess { get; private set; }
+
+        public RegisterViewModel(IAuthentication authentication)
+        {
+            _authentication = authentication;
+        }
+
+        private AsyncRelayCommand<object> _registerCommand;
+
+        public AsyncRelayCommand<object> RegisterCommand
+        {
+            get
+            {
+                return _registerCommand ??
+                       (_registerCommand = new AsyncRelayCommand<object>(async obj =>
+                       {
+                           try
+                           {
+                               var result = await _authentication.Register(Username, Email, Password);
+                               IsSuccess = result;
+                           }
+                           catch
+                           {
+                               IsSuccess = false;
+                           }
+                       }));
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
