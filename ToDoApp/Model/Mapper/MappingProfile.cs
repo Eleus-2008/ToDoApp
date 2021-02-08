99e9f1588a7fb703fe8d19168b4ecd44c0cb25e6
Anton Eliseev <anton2008.e@yandex.ru>
2021-02-08 20:07:24 +0400

    add automapper

---
+++ ToDoApp/Model/Mapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ToDoApp.Model.DTO;
+using ToDoAppWebService.DTO;
+
+namespace ToDoApp.Model.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Task, TaskDto>().ReverseMap();
+            CreateMap<ToDoList, ToDoListDto>().ReverseMap();
+        }
+    }
+}
